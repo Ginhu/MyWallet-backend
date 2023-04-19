63db1851a491f4c7ad713945f5aba8a6d7e372b9
Sergio Beppu <beppu.sergio@gmail.com>
2023-04-19 10:27:15 -0300

    POST /cadastro: Todos os requisitos mínimos implementados

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
+import joi from "joi"
+import bcrypt from "bcrypt"
 
 const app = express()
 app.use(express.json())
@@ -16,6 +18,31 @@ try {
     console.log(err.message)
 }
 const db = mongoClient.db()
+
+app.post("/cadastro", async (req, res) => {
+    const {name, email, password} = req.body
+    const signupSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(3).required()
+    })
+
+    const validation = signupSchema.validate(req.body, {abortEarly: false})
+    if (validation.error) return res.status(422).send("Existe um ou mais campos inválidos")
+
+    try {
+        const user = await db.collection("users").findOne({email})
+        if(user) return res.status(409).send("Email já cadastrado")
+
+        const encryptedPw = bcrypt.hashSync(password, 10)
+        await db.collection("users").insertOne({name, email, password: encryptedPw})
+        res.status(201).send("Cadastrado com sucesso!")
+
+    } catch (err) {
+        res.send(err.message)
+    }
+
+})
 
 const PORT = 5000
 app.listen(PORT, ()=>console.log(`SERVER ON na porta ${PORT}`))
