e2481ca9df1d92fe10241e7e74f2d3f8016fb352
Sergio Beppu <beppu.sergio@gmail.com>
2023-04-19 15:22:52 -0300

    POST /login: Todos os requisitos mínimos implementados

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 import joi from "joi"
 import bcrypt from "bcrypt"
+import { v4 as uuid } from "uuid"
 
 const app = express()
 app.use(express.json())
@@ -42,6 +43,30 @@ app.post("/cadastro", async (req, res) => {
         res.send(err.message)
     }
 
+})
+
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body
+    const loginSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    })
+
+    const validation = loginSchema.validate(req.body, {abortEarly: false})
+
+    if(validation.error) return res.status(422).send("Dados informados não estão no formato correto")
+
+    try {
+        const findUser = await db.collection("users").findOne({email})
+        if(!findUser) return res.status(404).send("Registro do usuário não encontrado")
+        if(!bcrypt.compareSync(password, findUser.password)) return res.status(401).send("Senha incorreta")
+
+        const token = uuid()
+        await db.collection("login").insertOne({userId: findUser._id, token: token})
+        res.status(200).send(token)
+    } catch (err) {
+        res.send(err.message)
+    }
 })
 
 const PORT = process.env.PORT || 5000
