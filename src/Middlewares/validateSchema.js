01520724356b9a7cf450fb0f687263fbf8985e14
Sergio Beppu <beppu.sergio@gmail.com>
2023-04-25 07:22:25 -0300

    correção de erros nos middleware

--- src/Middlewares/validateSchema.js
+++ src/Middlewares/validateSchema.js
@@ -1,7 +1,7 @@
 export function validateSchema(schema) {
 
     return (req, res, next) => {
-        const validation = signupSchema.validate(req.body, {abortEarly: false})
+        const validation = schema.validate(req.body, {abortEarly: false})
         if (validation.error) {
             const errors = validateSchema.error.details.map(details=>details.message)
             return res.status(422).send(errors)
@@ -13,7 +13,7 @@ export function validateSchema(schema) {
 
 export function validateTransactionSchema(schema) {
     return (req, res, next) => {
-        const validation = bodySchema.validate({value: value2, description, type, token})
+        const validation = schema.validate({value: value2, description, type, token})
         if(validation.error) {
             const errors = validateSchema.error.details.map(details=>details.message)
             return res.status(422).send(errors)
